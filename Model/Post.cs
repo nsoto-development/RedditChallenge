8d7506dac146de1ed2d01089c58e0975ae292f6c
Nelson Soto <nelson.soto.velez@gmail.com>
2024-05-03 20:13:32 -0400

    updated with more properties

--- Model/Post.cs
+++ Model/Post.cs
@@ -1,29 +1,40 @@
-using Reddit;
-
 namespace RedditChallenge.Model
 {
   public class Post
   {
+    public string PostId { get; set; } = "";
     public string Title { get; set; } = "";
     public string Author { get; set; } = "";
     public int Score { get; set; } = 0;
-    // public string Content { get; set; }
-    // public int Upvotes { get; set; }
-    // public int Downvotes { get; set; }
-    // public int CommentsCount { get; set; }
+    public string Permalink { get; set; } = "";
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Downvotes { get; set; } = 0;
+    public int Upvotes { get; set; } = 0;
+    public string Thumbnail { get; set; } = "";
+    public int? ThumbnailHeight { get; set; } = null;
+    public int? ThumbnailWidth { get; set; } = null;
 
     public Post()
     {
     }
     public Post(Reddit.Controllers.Post post)
     {
+      PostId = post.Id;
       Title = post.Title;
       Author = post.Author;
       Score = post.Score;
-      // Content = post.SelfText;
-      // Upvotes = post.UpVotes;
-      // Downvotes = post.DownVotes;
-      // CommentsCount = post.CommentCount;
+      Permalink = post.Permalink;
+      CreatedAt = post.Created;
+      Downvotes = post.DownVotes;
+      Upvotes = post.UpVotes;
+
+      // check if the post can be cast to a LinkPost - fill those extra properties if so
+      if (post is Reddit.Controllers.LinkPost linkPost)
+      {
+        Thumbnail = linkPost.Thumbnail;
+        ThumbnailHeight = linkPost.ThumbnailHeight;
+        ThumbnailWidth = linkPost.ThumbnailWidth;
+      }
     }
   }
 }
