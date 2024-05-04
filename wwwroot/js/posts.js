2aa3495e5b1d0238cc78c62c33f1d429b26cda0b
Nelson Soto <nelson.soto.velez@gmail.com>
2024-05-03 20:15:23 -0400

    websocket approach to async push post updated to client - wip

--- wwwroot/js/posts.js
+++ wwwroot/js/posts.js
@@ -1,41 +1,44 @@
-"use strict";
+// get the subreddit from the path in the url
+const subreddit = window.location.pathname.split("/")[2];
 
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/postsHub")
-    .build();
+if (subreddit) {
+  // Create a WebSocket connection
+  const webSocket = new WebSocket("ws://localhost:5051/redditUpdates?subreddit=" + encodeURIComponent(subreddit));
 
+  // When the WebSocket connection is opened
+  webSocket.onopen = function () {
+    console.log("WebSocket connection established.");
+  };
 
-connection.start()
-    .then(() => {
-        console.log("Connection started. Listening for new posts...");
-        // Start streaming new posts from the server
+  // When a message is received from the server
+  webSocket.onmessage = function (event) {
+    const updatedPosts = JSON.parse(event.data);
 
-        // get the subreddit name from the URL
-        const subreddit = window.location.pathname.split("/")[2];
+    // Handle updated posts and update the UI
+    const postList = document.getElementsByClassName("post-list")[0];
 
-        connection.stream("StreamNewPostsAsync", subreddit).subscribe({
-            next: post => {
-                console.log("Received new post:", post);
-                const postList = document.getElementsByClassName("post-list")[0];
-
-                const postInnerHtml = `
-                  <li class="post-item">
-                      <h3 class="post-title">${post.title}</h3>
-                      <div class="post-details">
-                          <span class="post-author">Author: ${post.author}</span>
-                          <span class="post-score">Score: ${post.score}</span>
-                      </div>
-                  </li>
+    // prepend the new posts to the top of the list
+    updatedPosts.forEach(function (post) {
+      const postInnerHtml = `
+                <li class="post-item">
+                    <h3 class="post-title">${post.title}</h3>
+                    <div class="post-details">
+                        <span class="post-author">Author: ${post.author}</span>
+                        <span class="post-score">Score: ${post.score}</span>
+                    </div>
+                </li>
                 `;
+      postList.insertAdjacentHTML("afterbegin", postInnerHtml);
+    });
+  };
 
-                postList.innerHTML = postInnerHtml + postList.innerHTML;
-            },
-            complete: () => {
-                console.log("Streaming of new posts completed.");
-            },
-            error: error => {
-                console.error("Error streaming new posts:", error);
-            }
-        });
-    })
-    .catch(err => console.error(err));
+  // When an error occurs with the WebSocket connection
+  webSocket.onerror = function (event) {
+    console.error("WebSocket error:", event);
+  };
+
+  // When the WebSocket connection is closed
+  webSocket.onclose = function (event) {
+    console.log("WebSocket connection closed:", event);
+  };
+}
